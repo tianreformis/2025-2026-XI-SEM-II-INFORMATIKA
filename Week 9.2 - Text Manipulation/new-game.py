daa28b8cd1d10ebea8280219fc2e4286bcbc57ea
tianreformis <tianreformis@gmail.com>
2026-03-30 15:01:57 +0800

    Adding : Velocity

--- Week 9.2 - Text Manipulation/new-game.py
+++ Week 9.2 - Text Manipulation/new-game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.mixer.init()
 pygame.init()
@@ -12,6 +13,7 @@ font = pygame.font.SysFont("Arial", 40)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 running = True
 pygame.mixer.music.load("music3.mp3")
 pygame.mixer.music.play(-1)  # Play music in a loop
@@ -19,9 +21,14 @@ pygame.mixer.music.set_volume(0.5)  # Set music volume (0.0 to 1.0)
 
 #event handling/layar
 status = "menu"
+
+#settings
 player_x = 400
 player_y = 300
 velocity = 0.05
+score = 0
+fruit_x = random.randint(50, WIDTH-10)
+fruit_y = random.randint(50, HEIGHT-10)
 
 #game loop
 while running: 
@@ -41,16 +48,18 @@ while running:
         screen.blit(text_exit, rect_exit)
 
     elif status =="playing":
-        # text_playing = font.render("Playing Game...!", True, WHITE)
-        # rect_playing = text_playing.get_rect(center=(400,300))
-        # screen.blit(text_playing, rect_playing)
+        text_score = font.render(f"Score : {score}", True, WHITE)
+        rect_score = text_score.get_rect(center=(400,50))
+        screen.blit(text_score, rect_score)
 
-        #renderObject
-        
-
+        #renderObject       
         text_player = font.render("o", True, WHITE)
         rect_player = text_player.get_rect(center=(player_x,player_y))
         screen.blit(text_player, rect_player)
+
+        text_fruit = font.render("+", True, YELLOW)
+        rect_fruit = text_fruit.get_rect(center=(fruit_x,fruit_y))
+        screen.blit(text_fruit, rect_fruit)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
